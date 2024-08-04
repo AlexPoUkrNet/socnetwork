48d4691a40c26f3226fb08468c9d3a3b0bec4bd5
Alex Po <alex_po@ukr.net>
2024-08-04 23:05:20 +0300

    Initial commit

---
+++ src/Components/Profile.js
@@ -0,0 +1,13 @@
+const Profile = () => {
+    return (
+            <div className="content">
+                <div>ava+description</div>
+                <div>my posts</div>
+                <div>new post</div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+    );
+}
+
+export default Profile;
